70e14f2dfd3e303d58de5fbd8458ae2148dc2675
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-01-04 10:51:49 +0100

    Add pizza to the app

--- PizzaClient/src/main.jsx
+++ PizzaClient/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import Pizza from './Pizza'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Pizza />
   </React.StrictMode>,
 )
